ebb1eebca6eb5a7b3822e80b4440a3024d7068a0
NatanFitness360Now <natan.furer@fitness360now.com>
2023-07-08 21:09:29 +0300

    comming soon page

---
+++ src/pages/ComingSoonPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles/ComingSoonPage.css';
+
+
+
+const ComingSoonPage = () => {
+  return (
+    <div className="coming-soon-page">
+      <img src='/uc.svg' alt='uc' className="background-image"/>
+      <div className="content">
+        <h1>Coming Soon</h1>
+        <p>We're working hard to bring you an amazing website.</p>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoonPage;
