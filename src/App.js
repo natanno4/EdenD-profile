d5899209cd8b8f0d199679539d11d6a75bebb9ad
NatanFitness360Now <natan.furer@fitness360now.com>
2023-07-08 21:09:04 +0300

    comming soon page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import ReactGA from "react-ga4";
-
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
 import Projects from "./pages/projects";
 import Contact from "./pages/contact";
 import Notfound from "./pages/404";
+import ComingSoonPage from "./pages/ComingSoonPage";
 
 import { TRACKING_ID } from "./data/tracking";
 import "./app.css";
@@ -17,7 +17,7 @@ function App() {
 			ReactGA.initialize(TRACKING_ID);
 		}
 	}, []);
-
+	/* 
 	return (
 		<div className="App">
 			<Routes>
@@ -28,7 +28,10 @@ function App() {
 				<Route path="*" element={<Notfound />} />
 			</Routes>
 		</div>
-	);
+	);*/
+	return (
+		<ComingSoonPage />
+	  );
 }
 
 export default App;
